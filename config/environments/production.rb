70754d5fada745318986c0effa8218fc2002eefb
Won June Tai <wonjune.tai@gmail.com>
2014-10-21 12:54:26 -0400

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,5 @@ Getducky::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = {:host =>'http://getducky.herokuapp.com/'}
 end
