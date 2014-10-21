82c1960de046568a81329b2c59320f92fe2bd4a3
Won June Tai <wonjune.tai@gmail.com>
2014-10-21 12:01:39 -0400

    Changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Getducky.Application.configure do
+Getducky::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
