ced218d232792fe27a3954b5606df92b297fb2b9
Won June Tai <wonjune.tai@gmail.com>
2014-10-24 02:52:18 -0400

    Fix no 2 for integer issue in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20141024061844) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.bigint   "contact"
+    t.integer  "contact"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
