0b89bf26cc04fc1661c8342100214b4161531b2e
Won June Tai <wonjune.tai@gmail.com>
2014-10-24 03:25:31 -0400

    One more try with using phone number as string for contact info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024061844) do
+ActiveRecord::Schema.define(version: 20141024072241) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20141024061844) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.integer  "contact"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
