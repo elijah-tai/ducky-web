acab3126532c898debc446d39dc2e24da34a8200
Won June Tai <wonjune.tai@gmail.com>
2014-10-24 03:08:10 -0400

    Still trying to fix integer issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024061844) do
+ActiveRecord::Schema.define(version: 20141024065700) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20141024061844) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.bigint   "contact"
+    t.integer  "contact"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
