5810e3c1ce3983c93605449a08116aaec18fca3e
Bazhunga <bazhunga@Bazhunga.(none)>
2014-10-29 02:08:19 -0400

    Wraparounds have been added. Also put location: and the location on separate lines for aesthetics

---
+++ app/helpers/index_helper.rb
@@ -0,0 +1,24 @@
+module IndexHelper
+	def format_output(req_desc) #Gets pin.description
+  	#The purpose of this function is to prevent overflow of the text
+  	#box due to extremely long words
+  	#This will be rare if the users aren't fudging up the spacebar
+  	#and are not deliberately trying to screw with things
+    
+    #The textbox can comfortably fit 16 m characters or like 50 i's
+    desc_array = req_desc.split(" ")
+    desc_array.ech do |word|
+    	
+    	mult16 = (word.length/16).floor #Length of word in multiples of 16. A 34 character word as a mult16 length of 2
+    	
+    	if mult16 > 0
+
+    		for i in 1..mult16
+    			word.insert(16*i, '-')
+   	 		end
+
+   	 	end
+    end
+    desc_array.join(" ")
+  end 
+end
--- app/helpers/pin_helper.rb
+++ app/helpers/pin_helper.rb
@@ -8,4 +8,28 @@ module PinHelper
     end 
 
   end 
+  
+  def format_output(req_desc) #Gets pin.description
+  	#The purpose of this function is to prevent overflow of the text
+  	#box due to extremely long words
+  	#This will be rare if the users aren't fudging up the spacebar
+  	#and are not deliberately trying to screw with things
+    
+    #The textbox can comfortably fit 16 m characters or like 50 i's
+    desc_array = req_desc.split(" ")
+    desc_array.each do |word|
+    	
+    	mult15 = (word.length/15).floor #Length of word in multiples of 16. A 34 character word as a mult16 length of 2
+    	
+    	if mult15 > 0
+
+    		for i in 1..mult15
+    			word.insert(15*i, "\n")
+   	 		end
+
+   	 	end
+    end
+    description = desc_array.join(" ")
+    return description
+  end 
 end
