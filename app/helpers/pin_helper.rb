4aa5a0a2fbaec9c9acebaea7f804f37bf7674568
Bazhunga <bazhunga@Bazhunga.(none)>
2014-10-29 02:11:12 -0400

    fixed comment typo

--- app/helpers/pin_helper.rb
+++ app/helpers/pin_helper.rb
@@ -15,11 +15,11 @@ module PinHelper
   	#This will be rare if the users aren't fudging up the spacebar
   	#and are not deliberately trying to screw with things
     
-    #The textbox can comfortably fit 16 m characters or like 50 i's
+    #The textbox can comfortably fit 15 m characters or like 50 i's
     desc_array = req_desc.split(" ")
     desc_array.each do |word|
     	
-    	mult15 = (word.length/15).floor #Length of word in multiples of 16. A 34 character word as a mult16 length of 2
+    	mult15 = (word.length/15).floor #Length of word in multiples of 15. A 34 character word as a mult15 length of 2
     	
     	if mult15 > 0
 
