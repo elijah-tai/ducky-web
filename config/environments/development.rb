9f1dd7d7404a298cdd35cafd96e87590e353e027
Won June Tai <wonjune.tai@gmail.com>
2014-10-30 15:07:59 -0400

    Prep for confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,17 +42,16 @@ Getducky::Application.configure do
   # config.action_mailer.raise_delivery_errors = true
 
   # # Set up delivery method for emails
-  # config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   
-
-  # config.action_mailer.smtp_settings = {
-  # address: “smtp.gmail.com”,
-  # port: 587,
-  # domain: ENV["GMAIL_DOMAIN"],
-  # authentication: “plain”,
-  # enable_starttls_auto: true,
-  # user_name: ENV["GMAIL_USERNAME"],
-  # password: ENV["GMAIL_PASSWORD"]
-  # }
+  ActionMailer::Base.delivery_method = :smtp
 
 end
