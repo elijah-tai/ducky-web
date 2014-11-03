157a2c678b14fb7f506f248ba7fafa5c7c87193d
Won June Tai <wonjune.tai@gmail.com>
2014-11-02 23:00:23 -0500

    Allow for multiple social networks on shared/links

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable,
-  :confirmable, :omniauthable, :omniauth_providers => [:facebook]
+  :confirmable, :omniauthable, :omniauth_providers => [:facebook],
 
   has_many :pins, dependent: :destroy
 
@@ -16,8 +16,9 @@ class User < ActiveRecord::Base
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
-    # user.name = auth.info.name   # assuming the user model has a name
+    user.name = auth.info.name   # assuming the user model has a name
     # user.image = auth.info.image # assuming the user model has an image
   end
+
 end
 end
